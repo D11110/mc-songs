841902bcf7f2083ce6d2967dd78f3e01e19cfc9c
D11110 <49168911+D11110@users.noreply.github.com>
2025-10-07 21:41:17 -0600

    Add files via upload

--- track1player.lua
+++ track1player.lua
@@ -1,11 +1,25 @@
 local dfpwm = require("cc.audio.dfpwm")
 local speaker = peripheral.find("speaker")
 
-local decoder = dfpwm.make_decoder()
-for chunk in io.lines("track1.dfpwm", 16 * 1024) do
-    local buffer = decoder(chunk)
+if not speaker then
+    error("No se encontró un speaker conectado.")
+end
 
+local decoder = dfpwm.make_decoder()
+local file = fs.open("track1.dfpwm", "rb")
+
+if not file then
+    error("No se pudo abrir track1.dfpwm")
+end
+
+while true do
+    local chunk = file.read(16 * 1024)
+    if not chunk then break end
+
+    local buffer = decoder(chunk)
     while not speaker.playAudio(buffer) do
         os.pullEvent("speaker_audio_empty")
     end
 end
+
+file.close()
